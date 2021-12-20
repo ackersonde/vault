84a9273c31ab660a129a5a920b938cb77065b08d
Dan Ackerson <dan@ackerson.de>
2021-12-20 12:29:36 +0100

    race condition sleep

--- init-vault.sh
+++ init-vault.sh
@@ -3,14 +3,14 @@
 # this should only be run in a uninitialized, new instance of vault
 # (e.g. volume doesn't have vault config/data in it)
 chown vault:vault /vault/
-vault operator init -key-shares=3 -key-threshold=2 > /vault/config/generated_keys.txt
+vault operator init -key-shares=3 -key-threshold=2 > generated_keys.txt
 
-keyArray=$(grep 'Unseal Key ' < /vault/config/generated_keys.txt  | cut -c15-)
+keyArray=$(grep 'Unseal Key ' < generated_keys.txt  | cut -c15-)
 for s in $keyArray; do
   vault operator unseal "$s"
 done
 unset keyArray
-export VAULT_TOKEN=$(grep "Initial Root Token: " < /vault/config/generated_keys.txt  | cut -c21-)
+export VAULT_TOKEN=$(grep "Initial Root Token: " < generated_keys.txt  | cut -c21-)
 
 # Enable kv
 vault secrets enable -version=2 kv
